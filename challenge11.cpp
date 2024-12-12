254299ab888b5b479f9cd087db1b6b65f5b83c26
eseabro <62852911+eseabro@users.noreply.github.com>
2024-12-12 22:42:14 +0100

    started day 11

---
+++ challenge11.cpp
@@ -0,0 +1,16 @@
+
+
+
+
+int blink(char input[], int len){
+    int output;
+    if (len==1 && input[0] == '0'){
+        output = 1;
+    }else if(sizeof(input)%2==0){// digits even
+        // split in two 
+    }else{
+        // 
+    }
+    return output;
+}
+
